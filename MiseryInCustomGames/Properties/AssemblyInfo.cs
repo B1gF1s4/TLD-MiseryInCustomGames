a68da8247ff5a4c215596b96e83f36020048cc1c
B1gF1s4 <120759854+B1gF1s4@users.noreply.github.com>
2024-07-08 22:06:41 +0200

    updates version number

--- MiseryInCustomGames/Properties/AssemblyInfo.cs
+++ MiseryInCustomGames/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("MiseryInCustomGames")]
 [assembly: AssemblyCopyright("B1gF1s4")]
-[assembly: AssemblyVersion("0.6.0")]
-[assembly: AssemblyFileVersion("0.6.0")]
-[assembly: MelonInfo(typeof(Mod), "MiseryInCustomGames", "0.6.0", "B1gF1s4", null)]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyFileVersion("0.7.0")]
+[assembly: MelonInfo(typeof(Mod), "MiseryInCustomGames", "0.7.0", "B1gF1s4", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
